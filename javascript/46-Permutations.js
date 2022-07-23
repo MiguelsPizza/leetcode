f8da4619fd05b71692cbaf9a814d823842abe5c1
Alex Nahas <alexmnahas@gmail.com>
2022-07-23 09:57:45 -0400

    A more readable solution with comments

--- javascript/46-Permutations.js
+++ javascript/46-Permutations.js
@@ -1,25 +1,20 @@
-function permute(nums) {
-    const res = [];
-
-    if (nums.length === 1) {
-        return [nums.slice()];
-    }
-
-    for (const i of nums) {
-        let n = nums.shift();
-
-        let perms = permute(nums);
-
-        for (const perm of perms) {
-            perm.push(n);
+/**
+ * @param {number[]} nums
+ * @return {number[][]}
+ */
+var permute = function(nums) {
+    const result = [];
+    
+    const rec = (arr =[], newNums = nums) => {
+        if(!newNums.length) result.push(arr)     //push the permutations array into the result array once it has reached a full permutation
+        
+        for(let i = 0; i < newNums.length; i++){
+            const temp = [...newNums]             //creat a copy of the array to mutate
+            temp.splice(i, 1)                     //remove the element you are adding to the permutation array
+            rec([...arr, newNums[i]], temp)       //call the recusive function on the updated permutation array and reduced numbers array
         }
-
-        perms.forEach((perm) => {
-            res.push(perm);
-        });
-
-        nums.push(n);
     }
-
-    return res;
-}
+    
+    rec() 
+    return result
+};
